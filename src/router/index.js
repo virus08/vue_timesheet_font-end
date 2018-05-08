849bc3182b87d29e43b94907f5a702122d7cdc62
Wanchai <Wanchai@WANCHAI.tvs>
2018-05-08 11:45:16 +0700

    Create login route Phase I

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import layout from '@/views/layout/layout'
+import login from '@/views/login/login'
+import NotFound from '@/views/NotFound'
+
+const routes = [
+  { path: '/', component: layout, meta: { requiresAuth: true } },
+  { path: '/login', component: login },
+  { path: '*', component: NotFound }
+]
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const router = new Router({
+  mode: 'history',
+  routes
 })
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isAuthenticated = 1
+  if (requiresAuth && !isAuthenticated) {
+    next('/signin')
+  } else {
+    next()
+  }
+})
+
+export default router
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'layout'
+}
+</script>
+
+<style>
+
+</style>
