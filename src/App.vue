2006750f9cf7196718fa7e3eff4b93845c931030
Wanchai (ES) Fuangmali <Wanchai@vstecs.co.th>
2018-05-13 09:58:27 +0700

    Debug @ login css table.css

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
 <style src="@/assets/font-awesome/css/font-awesome.css"></style>
 <style src="@/assets/css/animate.css"></style>
 <style src="@/assets/css/style.css"></style>
-<style src="@/assets/css/table-component.css"></style>
+
 
 
 <style>
